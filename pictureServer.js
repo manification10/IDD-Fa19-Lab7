3ff94ac0e3aed1deb5b19d54b3db2a3849092b32
Ananya Paul <manification10@users.noreply.github.com>
2019-11-01 19:16:25 -0400

    Add files via upload

--- pictureServer.js
+++ pictureServer.js
@@ -127,9 +127,35 @@ io.on('connect', function(socket) {
     /// The browser will take this new name and load the picture from the public folder.
     // open a file called "lenna.png"
   });
-
   });
-  
+  socket.on('editPicture', function() {
+  //   /// First, we create a name for the new picture.
+  //   /// The .replace() function removes all special characters from the date.
+  //   /// This way we can use it as the filename.
+  //   imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
+  //
+  //   console.log('making a making a picture at'+ imageName); // Second, the name is logged to the console.
+  //
+  //   //Third, the picture is  taken and saved to the `public/`` folder
+  //   NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
+  //   io.emit('newPicture',(imageName+'.jpg')); ///Lastly, the new name is send to the client web browser.
+  //   /// The browser will take this new name and load the picture from the public folder.
+  //   // open a file called "lenna.png"
+  // });
+
+
+// open a file called "lenna.png"
+Jimp.read('/public/'+imageName+'.jpg', (err, lenna) => {
+if (err) throw err;
+lenna
+  .resize(256, 256) // resize
+  .quality(60) // set JPEG quality
+  .greyscale() // set greyscale
+  .write('public/'+imageName+'-small-bw.jpg'); // save
+    });
+    io.emit('newPicture',(imageName+'-small-bw.jpg'));
+  });
+
 
   // if you get the 'disconnect' message, say the user disconnected
   socket.on('disconnect', function() {
